66b3a0eef99d351fae2db88f21fdb4d16909edb8
NehGuk <nurture.tumulus_0n@icloud.com>
2026-04-30 14:30:46 +0200

    Adds responsiveness to logo image

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import "./App.css"
 import logo from "./assets/logo.png"
-import Welcome from "./components/Welcome/Welcome"
+/* import Welcome from "./components/Welcome/Welcome"
+import Menu from "./components/Menu/Menu" */
 
 function App() {
   return (
     <>
+    <article className="logo-container">
     <img src={logo} className="logo-header"></img>
-    <Welcome />
-    
-
-      
+    </article>
+    {/* <Welcome />
+    <Menu /> */}
     </>
   )
 }
