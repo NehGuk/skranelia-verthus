eb09aac2b6615fa15986a0033e9be7ea5e71b1f8
NehGuk <nurture-tumulus_0n@icloud.com>
2026-04-30 19:33:10 +0200

    Updates .gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css"
 import logo from "./assets/logo.png"
-/* import Welcome from "./components/Welcome/Welcome"
-import Menu from "./components/Menu/Menu" */
+import Welcome from "./components/Welcome/Welcome"
+import Menu from "./components/Menu/Menu"
 
 function App() {
   return (
@@ -9,8 +9,8 @@ function App() {
     <article className="logo-container">
     <img src={logo} className="logo-header"></img>
     </article>
-    {/* <Welcome />
-    <Menu /> */}
+    <Welcome />
+    <Menu />
     </>
   )
 }
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,14 +1,16 @@
+import { menu } from "../../data/menu"
+
 function Menu() {
   return (
     <>
-    <h2>Vårmeny</h2>
-    <h3>Dish 1</h3>
-    <h3>Dish 2</h3>
-    <h3>Dish 3</h3>
-    <h3>Dish 4</h3>
-    <h3>Dish 5</h3>
-    <h3>Dish 6</h3>
-    <h3>Dish 7</h3>
+      <h2>Vårmeny</h2>
+      <h3>Dish 1</h3>
+      <h3>Dish 2</h3>
+      <h3>Dish 3</h3>
+      <h3>Dish 4</h3>
+      <h3>Dish 5</h3>
+      <h3>Dish 6</h3>
+      <h3>Dish 7</h3>
     </>
   )
 }
